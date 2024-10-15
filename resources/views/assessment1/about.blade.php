110d6159c2f83d3e0866943dcc0b4ef5e20f3479
rsydfrmnsyh <rasy.fir.12@gmail.com>
2024-10-15 22:30:18 +0700

    assessment 1

--- resources/views/assessment1/about.blade.php
+++ resources/views/assessment1/about.blade.php
@@ -3,7 +3,7 @@
 <div class="container mt-4">
     <section id="home" class="mb-4">
         <h2>contoh</h2>
-        <p>masukin konten disini, kalo ga gapapa, hapus aja, kalo perlu lebih copy paste lg</p>
+        <p>masukin konten disini,w kalo ga gapapa, hapus aja, kalo perlu lebih copy paste lg</p>
     </section>
 </div>
 
